a7b4365e20b2c57b7a994d81b8f401eadea3ca51
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2021-10-15 10:20:04 +0800

    To Git

---
+++ src/main/java/com/astrogrowth/configserver/ConfigurationServerApplication.java
@@ -0,0 +1,16 @@
+package com.astrogrowth.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+
+public class ConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationServerApplication.class, args);
+	}
+
+}
